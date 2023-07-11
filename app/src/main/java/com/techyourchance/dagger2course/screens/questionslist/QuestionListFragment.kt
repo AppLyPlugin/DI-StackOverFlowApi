2471003b0a0524f333c5ea5016ec9fcb4dd40a68
LBRBS <lysanderarbis@gmail.com>
2023-07-11 05:59:36 +0300

    05 - Journey to Dependency Injection in the Tutorial Application(4)

---
+++ app/src/main/java/com/techyourchance/dagger2course/screens/questionslist/QuestionListFragment.kt
@@ -0,0 +1,97 @@
+package com.techyourchance.dagger2course.screens.questionslist
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.techyourchance.dagger2course.questions.FetchQuestionDetailsUseCase
+import com.techyourchance.dagger2course.questions.FetchQuestionsUseCase
+import com.techyourchance.dagger2course.questions.Question
+import com.techyourchance.dagger2course.screens.common.ScreensNavigator
+import com.techyourchance.dagger2course.screens.common.dialogs.DialogsNavigator
+import com.techyourchance.dagger2course.screens.common.fragment.BaseFragment
+import com.techyourchance.dagger2course.screens.questiondetails.QuestionDetailsActivity
+import com.techyourchance.dagger2course.screens.questiondetails.QuestionDetailsViewMvc
+import kotlinx.coroutines.*
+
+class QuestionListFragment: BaseFragment(), QuestionsListViewMvc.Listener {
+
+    private val coroutineScope = CoroutineScope(SupervisorJob() + Dispatchers.Main.immediate)
+    private lateinit var viewMvc: QuestionsListViewMvc
+    private lateinit var questionId: String
+    private lateinit var fetchQuestionsUseCase: FetchQuestionsUseCase
+    private lateinit var dialogsNavigator: DialogsNavigator
+    private lateinit var screensNavigator: ScreensNavigator
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        fetchQuestionsUseCase = compositionRoot.fetchQuestionsUseCase
+        dialogsNavigator = compositionRoot.dialogsNavigator
+        screensNavigator = compositionRoot.screensNavigator
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        viewMvc = QuestionsListViewMvc(LayoutInflater.from(requireContext()), container)
+        return viewMvc.rootView
+    }
+
+    override fun onStart() {
+        super.onStart()
+        viewMvc.registerListener(this)
+        fetchQuestionDetails()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        coroutineScope.coroutineContext.cancelChildren()
+        viewMvc.unregisterListener(this)
+    }
+
+    private fun fetchQuestionDetails() {
+        coroutineScope.launch {
+            viewMvc.showProgressIndication()
+            try {
+                val result = fetchQuestionsUseCase.fetchLatestQuestions()
+                when(result) {
+                    is FetchQuestionsUseCase.Result.Success -> {
+                        viewMvc.bindQuestions(result.questions)
+                    }
+                    is FetchQuestionsUseCase.Result.Failure -> onFetchFailed()
+                }
+            } finally {
+                viewMvc.hideProgressIndication()
+            }
+        }
+    }
+
+    private fun onFetchFailed() {
+        dialogsNavigator.showServerErrorDialog()
+    }
+
+    companion object {
+        const val EXTRA_QUESTION_ID = "EXTRA_QUESTION_ID"
+        fun start(context: Context, questionId: String) {
+            val intent = Intent(context, QuestionDetailsActivity::class.java)
+            intent.putExtra(EXTRA_QUESTION_ID, questionId)
+            context.startActivity(intent)
+        }
+    }
+
+    override fun onRefreshClicked() {
+        fetchQuestionDetails()
+    }
+
+    override fun onQuestionClicked(clickedQuestion: Question) {
+        screensNavigator.toQuestionDetails(clickedQuestion.id)
+    }
+
+}
