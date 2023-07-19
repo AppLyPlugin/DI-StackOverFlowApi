a7d5a72cb2659b8a631fea5d75cc3f1e05441275
LBRBS <lysanderarbis@gmail.com>
2023-07-19 20:22:48 +0300

    08 - Additional Dagger Conventions(  002 Static Provider Methods and Component Builders )

--- app/src/main/java/com/techyourchance/dagger2course/MyApplication.kt
+++ app/src/main/java/com/techyourchance/dagger2course/MyApplication.kt
@@ -1,12 +1,13 @@
 package com.techyourchance.dagger2course
 
 import android.app.Application
+import com.techyourchance.dagger2course.common.dependencyinjection.app.AppComponent
 import com.techyourchance.dagger2course.common.dependencyinjection.app.AppModule
 import com.techyourchance.dagger2course.common.dependencyinjection.app.DaggerAppComponent
 
 class MyApplication: Application() {
 
-    public val appComponent by lazy {
+    public val appComponent: AppComponent by lazy {
         DaggerAppComponent.builder()
             .appModule(AppModule(this))
             .build()
--- app/src/main/java/com/techyourchance/dagger2course/common/dependencyinjection/activity/ActivityModule.kt
+++ app/src/main/java/com/techyourchance/dagger2course/common/dependencyinjection/activity/ActivityModule.kt
@@ -7,21 +7,16 @@ import dagger.Module
 import dagger.Provides
 
 @Module
-class ActivityModule(
-    val activity: AppCompatActivity
-) {
+object ActivityModule{
 
-    @Provides
-    fun activity() = activity
+        @Provides
+        @ActivityScope
+        fun screensNavigator(activity: AppCompatActivity) = ScreensNavigator(activity)
 
-    @Provides
-    @ActivityScope
-    fun screensNavigator(activity: AppCompatActivity) = ScreensNavigator(activity)
+        @Provides
+        fun layoutInflater(activity: AppCompatActivity) = LayoutInflater.from(activity)
 
-    @Provides
-    fun layoutInflater(activity: AppCompatActivity) = LayoutInflater.from(activity)
-
-    @Provides
-    fun fragmentManager(activity: AppCompatActivity) = activity.supportFragmentManager
-
+        @Provides
+        fun fragmentManager(activity: AppCompatActivity) = activity.supportFragmentManager
+    
 }
